d0bb8aec5726efdc841bc84fe5c973b43b8dff15
Taehyun Kim <kgyoo8232@gmail.com>
2019-08-14 15:27:02 +0900

    move table query to trait

--- src/main/scala/bookmark/repository/BookmarkRepository.scala
+++ src/main/scala/bookmark/repository/BookmarkRepository.scala
@@ -1,10 +1,36 @@
 package bookmark.repository
 
-import bookmark.domain.Bookmark
+import bookmark.Database
+import bookmark.domain.{Bookmark, BookmarkType}
 import com.google.inject.ImplementedBy
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
+import slick.lifted
+import slick.lifted.ProvenShape
 
 @ImplementedBy(classOf[BookmarkRepositoryImpl])
 trait BookmarkRepository {
+  val database: Database
+
+  import database.api._
+
+  implicit val bookmarkTypeMapper
+  : JdbcType[BookmarkType] with BaseTypedType[BookmarkType] =
+    MappedColumnType.base[BookmarkType, String](_.name, BookmarkType.valueOf)
+
+  class BookmarkTable(tag: Tag) extends Table[Bookmark](tag, "bookmarks") {
+    def id: Rep[String] = column[String]("id", O.PrimaryKey)
+    def ownerID: Rep[String] = column[String]("owner_id")
+    def bookmarkType: Rep[BookmarkType] = column[BookmarkType]("type")
+    def targetID: Rep[String] = column[String]("target_id")
+    def description: Rep[Option[String]] = column[Option[String]]("description")
+
+    override def * : ProvenShape[Bookmark] =
+      (id, ownerID, bookmarkType, targetID, description).mapTo[Bookmark]
+  }
+
+  val bookmarks = lifted.TableQuery[BookmarkTable]
+
   def insert(bookmark: Bookmark): Int
   def update(id: String, bookmark: Bookmark): Int
   def delete(id: String): Int
--- src/main/scala/bookmark/repository/BookmarkRepositoryImpl.scala
+++ src/main/scala/bookmark/repository/BookmarkRepositoryImpl.scala
@@ -1,11 +1,8 @@
 package bookmark.repository
 
 import bookmark.Database
-import bookmark.domain.{Bookmark, BookmarkType}
+import bookmark.domain.Bookmark
 import com.google.inject.{Inject, Singleton}
-import slick.ast.BaseTypedType
-import slick.jdbc.JdbcType
-import slick.lifted.ProvenShape
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -14,23 +11,6 @@ import scala.concurrent.duration.Duration
 class BookmarkRepositoryImpl @Inject()(val database: Database)
     extends BookmarkRepository {
   import database.api._
-
-  implicit val bookmarkTypeMapper
-    : JdbcType[BookmarkType] with BaseTypedType[BookmarkType] =
-    MappedColumnType.base[BookmarkType, String](_.name, BookmarkType.valueOf)
-
-  class BookmarkTable(tag: Tag) extends Table[Bookmark](tag, "bookmarks") {
-    def id: Rep[String] = column[String]("id", O.PrimaryKey)
-    def ownerID: Rep[String] = column[String]("owner_id")
-    def bookmarkType: Rep[BookmarkType] = column[BookmarkType]("type")
-    def targetID: Rep[String] = column[String]("target_id")
-    def description: Rep[Option[String]] = column[Option[String]]("description")
-
-    override def * : ProvenShape[Bookmark] =
-      (id, ownerID, bookmarkType, targetID, description).mapTo[Bookmark]
-  }
-
-  val bookmarks = TableQuery[BookmarkTable]
 
   override def insert(bookmark: Bookmark): Int =
     Await.result(database.db.run(bookmarks += bookmark), Duration.Inf)
